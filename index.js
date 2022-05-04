194b9804768a49c87548f92746ae21ac3654d258
volp99 <stefanovolpatti@yahoo.it>
2022-05-04 19:36:05 +0200

    update gitignore, remove server file and add comments

--- index.js
+++ index.js
@@ -1,35 +1,47 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = 3001;
-const sqlite3 = require("sqlite3").verbose();
-const path = require("path");
-const db_name = path.join(__dirname, "data", "sample.db");
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
+const db_name = path.join(__dirname, 'data', 'sample.db');
 
-app.get("/", (req, res) => {
-  res.send("test");
-});
 
+/**
+ * @description: This are the routes for the API
+ */
 app.listen(port, () => {
-  console.log("Connect to port 3001");
+	console.log(`Example app listening at http://localhost:${port}`);
 });
 
-app.get("/albums", (req, res) => {
-  const sql = "SELECT * FROM albums ORDER BY Title";
-  db.all(sql, [], (err, rows) => {
-    if (err) {
-      return console.error(err.message);
-    }
-    res.send(rows);
-  });
+app.get('/', (req, res) => {
+	res.send('Hello World!');
 });
 
+app.get('/albums', (req, res) => {
+	const sql = 'SELECT * FROM albums ORDER BY Title';
+	db.all(sql, [], (err, rows) => {
+		if (err) {
+			return console.error(err.message);
+		}
+		res.send(rows);
+	});
+});
+
+
+/**
+ * @description: This is the database connection
+ */
 const db = new sqlite3.Database(db_name, (err) => {
-  if (err) {
-    return console.error(err.message);
-  }
-  console.log("Successful connect to the database 'sample.db'");
+	if (err) {
+		return console.error(err.message);
+	}
+	console.log('Successful connect to the database \'sample.db\'');
 });
 
+
+/**
+ * @description: Create the query a Books table
+ */
 const sql_create = `CREATE TABLE IF NOT EXISTS Books (
 	Book_ID INTEGER PRIMARY KEY AUTOINCREMENT,
 	Title VARCHAR(100) NOT NULL,
@@ -37,9 +49,13 @@ const sql_create = `CREATE TABLE IF NOT EXISTS Books (
 	Comments TEXT
 );`;
 
+
+/**
+ * @description: Create the Books table
+ */
 db.run(sql_create, (err) => {
-  if (err) {
-    return console.log(err.message);
-  }
-  console.log("Successful creation of the Books table");
+	if (err) {
+		return console.log(err.message);
+	}
+	console.log('Successful creation of the Books table');
 });
